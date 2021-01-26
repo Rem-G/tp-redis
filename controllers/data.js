cd03c21b3675d356eb853b98ab3f8b43cdd8e12a
Rémi <remi@Remis-MacBook-Air.local>
2021-01-26 15:14:40 +0100

    Minor update

--- controllers/data.js
+++ controllers/data.js
@@ -5,7 +5,7 @@ client.on("error", function(error) {
   console.error(error);
 });
 
-let incrementToken = (req, res, token, d) => {
+const incrementToken = (req, res, token, d) => {
     client.get(token, function(err, token_value){
         if (token_value < 10){
             client.incr(token);
@@ -14,7 +14,6 @@ let incrementToken = (req, res, token, d) => {
             res.status(200).json(d);
         }
         else{
-            req.session.token = "";
             req.session.logged = false;
             res.status(400).json({error : "Vous avez utilisé votre quota de requêtes, veuillez attendre 10min avant de réessayer"});
         }
@@ -34,11 +33,9 @@ function connectToken(req, token){
         req.session.logged = false;
         return req;
     }
-
 }
 
 function readData(req, res) {
-
     let Data = require("../models/data");
     const token = req.header('Authorization').replace('Bearer ', '');
 
